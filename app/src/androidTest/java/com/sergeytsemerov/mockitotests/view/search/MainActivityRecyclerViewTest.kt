a68dc2b0074e8ef32eaf2c2c6a6cfc5e223cc47b
SergeyTsemerov <sergeytsemerov@gmail.com>
2022-01-29 20:56:03 +0300

    Added: Custom ViewAction for click performing; Done: Item checkBox test

--- app/src/androidTest/java/com/sergeytsemerov/mockitotests/view/search/MainActivityRecyclerViewTest.kt
+++ app/src/androidTest/java/com/sergeytsemerov/mockitotests/view/search/MainActivityRecyclerViewTest.kt
@@ -1,13 +1,17 @@
 package com.sergeytsemerov.mockitotests.view.search
 
+import android.view.View
 import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.UiController
+import androidx.test.espresso.ViewAction
 import androidx.test.espresso.action.ViewActions.*
 import androidx.test.espresso.contrib.RecyclerViewActions
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.sergeytsemerov.mockitotests.BuildConfig
 import com.sergeytsemerov.mockitotests.R
+import org.hamcrest.Matcher
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
@@ -76,6 +80,37 @@ class MainActivityRecyclerViewTest {
                         hasDescendant(withText("FullName: 42")),
                         click()
                     )
+                )
+        }
+    }
+
+    private fun tapOnItemWithId(id: Int) = object : ViewAction {
+        override fun getConstraints(): Matcher<View>? {
+            return null
+        }
+
+        override fun getDescription(): String {
+            return "Нажимаем на view с указанным id"
+        }
+
+        override fun perform(uiController: UiController, view: View) {
+            val v = view.findViewById(id) as View
+            v.performClick()
+        }
+    }
+
+    @Test
+    fun activitySearch_PerformCustomClick() {
+        if (BuildConfig.TYPE == MainActivity.FAKE) {
+            loadList()
+
+            onView(withId(R.id.recyclerView))
+                .perform(
+                    RecyclerViewActions
+                        .actionOnItemAtPosition<SearchResultAdapter.SearchResultViewHolder>(
+                            0,
+                            tapOnItemWithId(R.id.checkbox)
+                        )
                 )
         }
     }
